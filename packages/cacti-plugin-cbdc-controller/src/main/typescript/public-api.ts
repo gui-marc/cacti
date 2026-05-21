f6ed38f65bc7cd73b6e27eb5455672cc729b7165
Guilherme Marcondes <guiherme.marcondes@tecnico.ulisboa.pt>
2026-05-21 13:48:53 +0100

    fix(cbdc): fix building package

--- packages/cacti-plugin-cbdc-controller/src/main/typescript/public-api.ts
+++ packages/cacti-plugin-cbdc-controller/src/main/typescript/public-api.ts
@@ -6,3 +6,42 @@ export async function createPluginFactory(
 ): Promise<PluginFactoryCBDCController> {
   return new PluginFactoryCBDCController(pluginFactoryOptions);
 }
+
+export * from "./generated/openapi/typescript-axios";
+
+export { PluginFactoryCBDCController } from "./plugin-factry-cbdc";
+
+export { PluginCBDCController, IPluginCBDCOptions } from "./plugin-cbdc";
+
+export {
+  InMemoryTransactionStore,
+  TransactionStore,
+} from "./store/transaction-store";
+
+export {
+  InMemoryComplianceProvidersStore,
+  ComplianceProvidersStore,
+} from "./store/compliance-providers-store";
+
+export {
+  FXProvisionStrategy,
+  FXQuote,
+  DynamicRange,
+} from "./core/fx-provision";
+
+export {
+  ILedgerEnvironment,
+  IInfrastructure,
+  IRequestOptions,
+  IInitiateTransactionRequest,
+  IAcceptTransactionRequest,
+  InitiateTransactionResult,
+  ITransaction,
+  TransactionStatus,
+  ComplianceResult,
+  IComplianceProvider,
+  IFXProvider,
+  IGetFXRateRequest,
+  IGetFXRateResponse,
+  IGetComplianceCheckResponse,
+} from "./types";
